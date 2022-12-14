f39c015201d2b5be9a18edbcb049844e78946a10
Jay K <jay94ks@gmail.com>
2022-12-14 19:50:44 +0900

    Fixed: Version informations and preparing to build nupkgs

--- nuget_collect.bat
+++ nuget_collect.bat
@@ -4,5 +4,6 @@ mkdir Nuget
 copy .\NIdentity.Core\bin\Release\*.nupkg .\Nuget
 copy .\NIdentity.Core.X509\bin\Release\*.nupkg .\Nuget
 copy .\NIdentity.Connector\bin\Release\*.nupkg .\Nuget
+copy .\NIdentity.Connector.AspNetCore\bin\Release\*.nupkg .\Nuget
 
 pause
