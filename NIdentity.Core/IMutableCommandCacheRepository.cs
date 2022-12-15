818bf92ed025b22c6118e06483059b88575f3d33
Jay K <jay94ks@gmail.com>
2022-12-15 23:15:36 +0900

    Impl: added comment and `UnsetAsync` method to IMutableCommandCacheRepository

--- NIdentity.Core/IMutableCommandCacheRepository.cs
+++ NIdentity.Core/IMutableCommandCacheRepository.cs
@@ -1,5 +1,8 @@
 ﻿namespace NIdentity.Core
 {
+    /// <summary>
+    /// Mutable command cache repository.
+    /// </summary>
     public interface IMutableCommandCacheRepository
     {
         /// <summary>
@@ -13,5 +16,14 @@
         /// <param name="Token"></param>
         /// <returns></returns>
         Task<bool> SetAsync(Type Kind, string Key, string Value, TimeSpan? ExpiresAfter = null, CancellationToken Token = default);
+
+        /// <summary>
+        /// Unset the command cache immediately.
+        /// </summary>
+        /// <param name="Kind"></param>
+        /// <param name="Key"></param>
+        /// <param name="Token"></param>
+        /// <returns></returns>
+        Task<bool> UnsetAsync(Type Kind, string Key, CancellationToken Token = default);
     }
 }
