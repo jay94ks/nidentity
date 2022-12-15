db7ac34a40b309bdd2ec97e8ea0444bd74c416f9
Jay K <jay94ks@gmail.com>
2022-12-15 23:13:52 +0900

    Impl: added command cache interface to optimize command executions.

---
+++ NIdentity.Core/ICommandCacheRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace NIdentity.Core
+{
+    /// <summary>
+    /// Abstracted command cache repository.
+    /// </summary>
+    public interface ICommandCacheRepository
+    {
+        /// <summary>
+        /// Get the command cache by its kind asyncrhonously.
+        /// </summary>
+        /// <param name="Kind"></param>
+        /// <param name="Key"></param>
+        /// <param name="Token"></param>
+        /// <returns></returns>
+        Task<string> GetAsync(Type Kind, string Key, CancellationToken Token = default);
+    }
+}
---
+++ NIdentity.Core/IMutableCommandCacheRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace NIdentity.Core
+{
+    public interface IMutableCommandCacheRepository
+    {
+        /// <summary>
+        /// Set the command cache by its kind asyncrhonously.
+        /// And the cache will be expired after specified timespan.
+        /// </summary>
+        /// <param name="Kind"></param>
+        /// <param name="Key"></param>
+        /// <param name="Value"></param>
+        /// <param name="ExpiresAfter"></param>
+        /// <param name="Token"></param>
+        /// <returns></returns>
+        Task<bool> SetAsync(Type Kind, string Key, string Value, TimeSpan? ExpiresAfter = null, CancellationToken Token = default);
+    }
+}
